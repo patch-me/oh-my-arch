f98ec85341e662ec72fcb308038ff028e00c2509
Brioche <thomasderudder@sfr.fr>
2024-02-24 01:41:48 +0100

    setup color and big up the size of the terminal

--- suckless/st/config.h
+++ suckless/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -95,28 +95,40 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-    /* 8 normal colors */
-    "black", "red3", "green3", "yellow3", "blue2", "magenta3", "cyan3",
-    "gray90",
+	/* 8 normal colors */
+	"#1a1b26",
+	"#f7768e",
+	"#9ece6a",
+	"#e0af68",
+	"#7aa2f7",
+	"#f38ba8",
+	"#7dcfff",
+	"#a9b1d6",
 
-    /* 8 bright colors */
-    "gray50", "red", "green", "yellow", "#5c5cff", "magenta", "cyan", "white",
+	/* 8 bright colors */
+	"#414868",
+	"#f7768e",
+	"#9ece6a",
+	"#e0af68",
+	"#7aa2f7",
+	"#f38ba8",
+	"#7dcfff",
+	"#a9b1d6",
 
-    [255] = 0,
+[256] = "#cdd6f4", /* default foreground colour */
+[257] = "#1a1b26", /* default background colour */
+[258] = "#cdd6f4", /*575268*/
 
-    /* more colors can be added after 255 to use with DefaultXX */
-    "#cccccc", "#555555", "gray90", /* default foreground colour */
-    "black",                        /* default background colour */
 };
 
+
 /*
- * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
+unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 258;
 
 /*
  * Default shape of cursor
